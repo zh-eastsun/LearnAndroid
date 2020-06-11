d71c1f05754b5c27034a3a02500e88148cf66336
张东阳 <zhangdongyang@pwrd.com>
2020-06-11 16:28:37 +0800

    更新sdk版本

--- app/src/main/java/com/personal/zdy/learnandroid/net/RequestApi.kt
+++ app/src/main/java/com/personal/zdy/learnandroid/net/RequestApi.kt
@@ -7,6 +7,6 @@ package com.personal.zdy.learnandroid.net
 interface RequestApi {
     companion object{
         val BASE_URL = "https://www.wanandroid.com"
-        val SPLASH_URL = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587392603440&di=79e4c4ee119a1d668328646bd0d2da6b&imgtype=0&src=http%3A%2F%2Fjn.android.tedu.cn%2Fimg%2F201703%2F1489134922211.png"
+        val SPLASH_URL = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1591874081786&di=a989d7a17408ed736c819538b1826915&imgtype=0&src=http%3A%2F%2Fvww.taskcity.com%2Ffiles%2F0006%2F2853%2Fandroid_logo.gif"
     }
 }
