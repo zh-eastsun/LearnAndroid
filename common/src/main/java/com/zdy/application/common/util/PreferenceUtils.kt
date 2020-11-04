eccba5b03b9aab469ba95c20c42f04c6c02fef49
zhangdongyang <zh_eastsun@163.com>
2020-11-04 20:00:54 +0800

    添加写入和读取用户是否登陆字段的逻辑

--- common/src/main/java/com/zdy/application/common/util/PreferenceUtils.kt
+++ common/src/main/java/com/zdy/application/common/util/PreferenceUtils.kt
@@ -10,17 +10,28 @@ import android.content.Context
  */
 object PreferenceUtils {
 
-    const val SP_NAME = "PRIVATE_DATA"
-    const val USERNAME_KEY = "USERNAME_KEY"
-    const val PASSWORD_KEY = "PASSWORD_KEY"
-    const val COOKIES_KEY = "COOKIE_KEY"
+    const val SP_NAME = "LOVE_ANDROID_SP_NAME" // sp名
+    const val USERNAME_KEY = "USERNAME_KEY"    // 用户名的键值
+    const val PASSWORD_KEY = "PASSWORD_KEY"    // 密码的键值
+    const val COOKIES_KEY = "COOKIE_KEY"       // 存储用户cookie的键值
+    const val IS_LOGINED = "IS_LOGIN_KEY"      // 用户是否登陆的标记位
 
     fun putString(context: Context, key: String, data: String) {
         context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE).edit().putString(key, data)
-            .commit()
+            .apply()
+    }
+
+    fun putBoolean(context: Context, key: String, data: Boolean) {
+        context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE).edit().putBoolean(key, data)
+            .apply()
     }
 
     fun getString(context: Context, key: String) {
         context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE).getString(key, "")
     }
+
+    fun getBoolean(context: Context, key: String) {
+        context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE).getBoolean(key, false)
+    }
+
 }
