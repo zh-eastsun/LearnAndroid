7438d4c6048e1fe1750126f78d100d362cfe064f
zhangdongyang <zh_eastsun@163.com>
2020-11-23 20:41:41 +0800

    添加主页状态

--- app/src/main/java/com/zdy/application/learnandroid/mvvm/home/HomeActivity.kt
+++ app/src/main/java/com/zdy/application/learnandroid/mvvm/home/HomeActivity.kt
@@ -1,6 +1,5 @@
 package com.zdy.application.learnandroid.mvvm.home
 
-import androidx.databinding.DataBindingUtil
 import com.zdy.application.common.base.mvvm.BaseView
 import com.zdy.application.learnandroid.R
 import com.zdy.application.learnandroid.databinding.ActivityHomeBinding
@@ -14,12 +13,31 @@ import com.zdy.application.learnandroid.databinding.ActivityHomeBinding
 
 class HomeActivity : BaseView() {
 
+    private val binding by lazy {
+        ActivityHomeBinding.inflate(layoutInflater)
+    }
+
     override fun otherOperate() {
-        TODO("Not yet implemented")
+
     }
 
     override fun bindView() {
-        ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    /**
+     * 清除所有选中状态
+     */
+    fun clearSelection() {
+        binding.homePageImage.setImageResource(R.drawable.home_icon_unselected)
+        binding.systemPageImage.setImageResource(R.drawable.system_icon_unselected)
+        binding.squarePageImage.setImageResource(R.drawable.square_icon_unselected)
+        binding.personalPageImage.setImageResource(R.drawable.personal_icon_unselected)
+
+        binding.homePageText.setTextColor(resources.getColor(R.color.sugar_blue))
+        binding.systemPageText.setTextColor(resources.getColor(R.color.sugar_blue))
+        binding.squarePageText.setTextColor(resources.getColor(R.color.sugar_blue))
+        binding.personalPageText.setTextColor(resources.getColor(R.color.sugar_blue))
     }
 
 }
