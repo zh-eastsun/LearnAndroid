13ba2ceebf8745a233b1553a5788edab0546b8af
zhangdongyang <zh_eastsun@163.com>
2020-12-01 20:24:42 +0800

    添加Banner数据类和适配器

---
+++ app/src/main/java/com/zdy/application/learnandroid/adapter/HomeBannerAdapter.kt
@@ -0,0 +1,19 @@
+package com.zdy.application.learnandroid.adapter
+
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.youth.banner.adapter.BannerAdapter
+
+/**
+ * Created by Android Studio.
+ * User: zh.eastsun
+ * Date: 12/1/20
+ * Time: 7:56 PM
+ */
+
+class HomeBannerAdapter(val data: String) {
+
+    inner class BannerViewHolder(val itemView: ImageView) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
---
+++ app/src/main/java/com/zdy/application/learnandroid/bean/BannerData.kt
@@ -0,0 +1,25 @@
+package com.zdy.application.learnandroid.bean
+
+/**
+ * Created by Android Studio.
+ * User: zh.eastsun
+ * Date: 12/1/20
+ * Time: 8:14 PM
+ */
+
+data class BannerData(
+    val `data`: List<ItemData>,
+    val errorCode: Int,
+    val errorMsg: String
+)
+
+data class ItemData(
+    val desc: String,
+    val id: Int,
+    val imagePath: String,
+    val isVisible: Int,
+    val order: Int,
+    val title: String,
+    val type: Int,
+    val url: String
+)
