24c31748f6991d5a10d0f83e1d3609123ba85f26
zhangdongyang <zh_eastsun@163.com>
2020-12-04 16:13:17 +0800

    添加Hilt相关

--- common/src/main/java/com/zdy/application/common/base/BaseApplication.kt
+++ common/src/main/java/com/zdy/application/common/base/BaseApplication.kt
@@ -3,11 +3,14 @@ package com.zdy.application.common.base
 import android.app.Application
 import android.content.Context
 import com.zdy.application.common.util.LogUtil
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * @author zhangdongyang
  * @date 2020/04/14
  */
+
+@HiltAndroidApp
 open class BaseApplication : Application() {
 
     override fun attachBaseContext(base: Context?) {
@@ -18,9 +21,5 @@ open class BaseApplication : Application() {
 
     private fun initLogUtil() {
         LogUtil.init("Learn Android", true)
-    }
-
-    override fun onCreate() {
-        super.onCreate()
     }
 }
