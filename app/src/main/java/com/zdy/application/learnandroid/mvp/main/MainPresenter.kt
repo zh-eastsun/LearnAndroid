5f80fb2719181f3bbed7c61179a530bdbfb28f9b
zhangdongyang <zh_eastsun@163.com>
2020-12-08 14:09:37 +0800

    删除无用的类

--- app/src/main/java/com/zdy/application/learnandroid/mvp/main/MainPresenter.kt
+++
@@ -1,7 +0,0 @@
-package com.zdy.application.learnandroid.mvp.main
-
-import android.content.Context
-import com.zdy.application.common.base.mvp.BasePresenter
-import com.zdy.application.common.base.mvp.IPresenter
-
-class MainPresenter(val context: Context) : BasePresenter(), IPresenter
